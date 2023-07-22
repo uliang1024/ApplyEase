bc700b0b4518265b4103daff91b0ba4eb3f6cd3a
uliang <amisleo000@gmail.com>
2023-07-22 15:58:50 +0800

    index page : home完成

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,42 +1,32 @@
-gsap
-  .timeline({
-    scrollTrigger: {
-      trigger: ".scrollDist",
-      start: "top top",
-      end: "bottom bottom",
-      scrub: 1,
-    },
-  })
-  .fromTo(".sky", { y: 0 }, { y: -100 }, 0)
-  .fromTo(".cloud1", { y: 100 }, { y: -800 }, 0)
-  .fromTo(".cloud2", { y: -150 }, { y: -500 }, 0)
-  .fromTo(".cloud3", { y: -50 }, { y: -650 }, 0)
-  .fromTo(".mountBg", { y: -10 }, { y: -100 }, 0)
-  .fromTo(".mountMg", { y: -30 }, { y: -250 }, 0)
-  .fromTo(".mountFg", { y: -50 }, { y: -600 }, 0);
+// 使用 gsap
+gsap.timeline({
+  scrollTrigger: {
+    trigger: ".scrollDist",
+    start: "top top",
+    end: "bottom bottom",
+    scrub: 1,
+  },
+})
+.fromTo(".sky", { y: 0 }, { y: -100 }, 0)
+.fromTo(".cloud1", { y: 100 }, { y: -800 }, 0)
+.fromTo(".cloud2", { y: -150 }, { y: -500 }, 0)
+.fromTo(".cloud3", { y: -50 }, { y: -650 }, 0)
+.fromTo(".mountBg", { y: -10 }, { y: -100 }, 0)
+.fromTo(".mountMg", { y: -30 }, { y: -250 }, 0)
+.fromTo(".mountFg", { y: -50 }, { y: -600 }, 0);
 
-$("#arrowBtn").on("mouseenter", (e) => {
-  gsap.to(".arrow", {
-    y: 10,
-    duration: 0.8,
-    ease: "back.inOut(3)",
-    overwrite: "auto",
-  });
-});
+// 文字打字動畫
+const holder = document.getElementById("holder");
+const text = "我們是景承科技T大使實習團隊 ~";
+let index = 0;
 
-$("#arrowBtn").on("mouseleave", (e) => {
-  gsap.to(".arrow", {
-    y: 0,
-    duration: 0.5,
-    ease: "power3.out",
-    overwrite: "auto",
-  });
-});
+function typingAnimation() {
+  if (index < text.length) {
+    holder.textContent += text.charAt(index);
+    index++;
+    setTimeout(typingAnimation, 80);
+  }
+}
 
-$("#arrowBtn").on("click", (e) => {
-  gsap.to(window, {
-    scrollTo: innerHeight,
-    duration: 0.1,
-    ease: "power1.inOut",
-  });
-});
+typingAnimation();
+
